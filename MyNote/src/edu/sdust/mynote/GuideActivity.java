8b0911c9652171beb784b7e1890bf1a89cdea51f
岳晨旭 <chishy@163.com>
2013-03-24 11:15:01 +0800

    v1.0.1

---
+++ MyNote/src/edu/sdust/mynote/GuideActivity.java
@@ -0,0 +1,122 @@
+package edu.sdust.mynote;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.sdust.mynote.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.ViewTreeObserver.OnPreDrawListener;
+import android.view.Window;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+
+public class GuideActivity extends Activity {
+
+	private static final int TO_THE_END = 0;// 到达最后一张
+	private static final int LEAVE_FROM_END = 1;// 离开最后一张
+
+	private int[] ids = { R.drawable.guide_1, R.drawable.guide_2,
+			R.drawable.guide_3, R.drawable.guide_4, 
+			R.drawable.guide_5, R.drawable.guide_6 };
+			
+	private List<View> guides = new ArrayList<View>();
+	private ViewPager pager;
+	private ImageView openImage;
+	private ImageView curDot;
+	private int offset;// 位移量
+	private int curPos = 0;// 记录当前的位置
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.guide_layout);
+
+		for (int i = 0; i < ids.length; i++) {
+			ImageView iv = new ImageView(this);
+			iv.setImageResource(ids[i]);
+			ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+					ViewGroup.LayoutParams.FILL_PARENT,
+					ViewGroup.LayoutParams.FILL_PARENT);
+			iv.setLayoutParams(params);
+			iv.setScaleType(ScaleType.FIT_XY);
+			guides.add(iv);
+		}
+
+		curDot = (ImageView) findViewById(R.id.cur_dot);
+		curDot.getViewTreeObserver().addOnPreDrawListener(
+				new OnPreDrawListener() {
+					public boolean onPreDraw() {
+						offset = curDot.getWidth();
+						return true;
+					}
+				});
+
+		GuidePagerAdapter adapter = new GuidePagerAdapter(guides);
+		pager = (ViewPager) findViewById(R.id.contentPager);
+		pager.setAdapter(adapter);
+		pager.setOnPageChangeListener(new OnPageChangeListener() {
+			public void onPageSelected(int arg0) {
+				moveCursorTo(arg0);
+				if (arg0 == ids.length - 1) {// 到最后一张了
+					handler.sendEmptyMessageDelayed(TO_THE_END, 500);
+				} else if (curPos == ids.length - 1) {
+					handler.sendEmptyMessageDelayed(LEAVE_FROM_END, 100);
+				}
+				curPos = arg0;
+			}
+
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			public void onPageScrollStateChanged(int arg0) {
+			}
+			
+		});
+		
+		openImage = (ImageView)this.findViewById(R.id.open);
+		openImage.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent openIntent=new Intent(GuideActivity.this,LoginActivity.class);
+				startActivity(openIntent);
+			}
+			});
+	}
+
+	/**
+	 * 移动指针到相邻的位置
+	 * 
+	 * @param position
+	 * 		  		指针的索引值
+	 * */
+	private void moveCursorTo(int position) {
+		TranslateAnimation anim = new TranslateAnimation(offset*curPos, offset*position, 0, 0);
+		anim.setDuration(300);
+		anim.setFillAfter(true);
+		curDot.startAnimation(anim);
+	}
+
+	Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			if (msg.what == TO_THE_END)
+				openImage.setVisibility(View.VISIBLE);
+			else if (msg.what == LEAVE_FROM_END)
+				openImage.setVisibility(View.GONE);
+		}
+	};
+}
---
+++ MyNote/src/edu/sdust/mynote/LoginActivity.java
@@ -0,0 +1,118 @@
+package edu.sdust.mynote;
+import edu.sdust.mynote_menu.AboutUsActivity;
+import edu.sdust.mynote_menu.FeedbackActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class LoginActivity extends Activity {
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+       
+        
+    ImageButton imageButton=(ImageButton)this.findViewById(R.id.login_option);
+    imageButton.setOnClickListener(new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			openOptionsMenu();  
+		}
+	
+    });
+    
+    Button loginButton=(Button)this.findViewById(R.id.login_btn_login);
+    loginButton.setOnClickListener(new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent mainIntent=new Intent(LoginActivity.this,MainActivity.class);
+			startActivity(mainIntent);
+		}
+    	
+    });
+    
+    }
+    
+    //对菜单进行重写
+    @Override  
+	public boolean onCreateOptionsMenu(Menu menu) {
+		  menu.add(Menu.NONE, Menu.FIRST + 1, 1, "意见反馈");
+		 // setIcon()方法为菜单设置图标，这里使用的是系统自带的图标，留意一下,以      
+		 // android.R开头的资源是系统提供的，我们自己提供的资源是以R开头的        
+		  menu.add(Menu.NONE, Menu.FIRST + 2, 2, "关于我们"); 
+		return true;  
+		        }   
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) 
+		{        
+		case Menu.FIRST + 1: 			
+			Intent feedback =new Intent(LoginActivity.this,FeedbackActivity.class);
+			startActivity(feedback); 
+			break;
+		case Menu.FIRST + 2: 
+			Intent aboutUs =new Intent(LoginActivity.this,AboutUsActivity.class);
+			startActivity(aboutUs); 
+			break;     
+		} 
+		return false;  
+		}
+    
+    @Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if (keyCode == KeyEvent.KEYCODE_BACK )
+		{
+			// 创建退出对话框
+			AlertDialog isExit = new AlertDialog.Builder(this).create();
+			// 设置对话框标题
+			isExit.setTitle("系统提示");
+			// 设置对话框消息
+			isExit.setMessage("确定要退出吗");
+			// 添加选择按钮并注册监听
+			isExit.setButton("确定", listener);
+			isExit.setButton2("取消", listener);
+			// 显示对话框
+			isExit.show();
+
+		}
+		//屏蔽菜单按钮，为实现自定义菜单按钮
+		if(keyCode==KeyEvent.KEYCODE_MENU)
+			return true;
+		
+		return false;
+		
+	}
+	/**监听对话框里面的button点击事件*/
+	DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener()
+	{
+		public void onClick(DialogInterface dialog, int which)
+		{
+			switch (which)
+			{
+			case AlertDialog.BUTTON_POSITIVE:// "确认"按钮退出程序
+				finish();
+				break;
+			case AlertDialog.BUTTON_NEGATIVE:// "取消"第二个按钮取消对话框
+				break;
+			default:
+				break;
+			}
+		}
+	};	
+
+}
