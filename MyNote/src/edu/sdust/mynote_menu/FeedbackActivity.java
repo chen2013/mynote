d546bab1d620e9f218960b6683fc326033f83fc3
岳晨旭 <chishy@163.com>
2013-03-24 11:28:00 +0800

    Revert "Revert "v1.0.1""
    This reverts commit b5f026f26d12c59eb40da8cd6e5cdb29e5aeb25b.

---
+++ MyNote/src/edu/sdust/mynote_menu/FeedbackActivity.java
@@ -0,0 +1,17 @@
+package edu.sdust.mynote_menu;
+
+import edu.sdust.mynote.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeedbackActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.feedback_layout);
+	}
+
+	
+}
